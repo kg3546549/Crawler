3cdaa735d7235d78a6a6910152d69a1f10ce07fc
Geon <kg354654932@gmail.com>
2022-10-10 15:50:55 +0900

    Toon List to JSON, Toon Ranking to JSON

---
+++ JS/Ranking.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const dayList = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+let toonList = [];
+
+axios.get('https://comic.naver.com/webtoon/weekday')
+    .then(function(response) {
+        const $ = cheerio.load(response.data);
+        //전체 주들의 DOM을 가지고 있음
+        const $weekIdx = $('#content > div.list_area.daily_all > div.col');
+
+        for(let weeks = 1;weeks<=$($weekIdx).length;weeks++) {
+            //toonList에 Push할 배열
+            let dayToonList = [];
+
+            const weekToonIdx = $($weekIdx[weeks-1]).find('li').length;
+            console.log('일 별 웹툰 갯수 : '+weekToonIdx);
+            for(let i=1;i<=weekToonIdx;i++) {
+                //각 li에 참조
+                let selectorString = '#content > div.list_area.daily_all > div:nth-child('+ weeks +') > div > ul > li:nth-child('+ i +') > a';
+                const $name = $(selectorString);
+
+                dayToonList.push({
+                    idx : i,
+                    title : $($name).text(),
+                });
+            }
+            toonList.push(
+                {
+                    day : dayList[weeks-1],
+                    numOfWebtoons : weekToonIdx,
+                    webToons : dayToonList
+                }
+            );
+        }
+        
+        //JSON Writing
+        const JSON_weekToonList = JSON.stringify(toonList);
+        fs.writeFileSync('first-json.json',JSON_weekToonList);
+        
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    .then(function() {
+        
+    });
---
+++ JS/allToons.js
@@ -0,0 +1,69 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const baseURL = 'https://comic.naver.com';
+const AlphabetList = ['ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','A','0'];
+
+let allToonList = {
+    numOfAllToons : 0,
+    baseURL : baseURL,
+    toonList : [],
+};
+
+axios.get('https://comic.naver.com/webtoon/creation')
+    .then(function(response) {
+        const $ = cheerio.load(response.data);
+        
+        const $toons = $('#content > div.all_list.all_image > div.section');
+        
+        //알파벳 갯수는 16개(ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎA0)
+        
+        for(let alphabet = 0;alphabet<$toons.length;alphabet++) {
+            const numOfToons = $($toons[alphabet]).find('li').length;
+
+            let ToonList = {
+                alphabet : AlphabetList[alphabet],
+                numOfToons : numOfToons,
+                toonList : [],
+            };
+
+            console.log(AlphabetList[alphabet]+'의 웹툰 개수 : ' + numOfToons);
+            allToonList.numOfAllToons += numOfToons;
+
+            for(let index = 1;index < numOfToons; index++) {
+                const selectorString = '#content > div.all_list.all_image > div:nth-child('
+                +(alphabet+1)+') > ul > li:nth-child('+(index)+') > div > a';
+
+                const imgSelectorString = '#content > div.all_list.all_image > div:nth-child('
+                +(alphabet+1)+') > ul > li:nth-child('+(index)+') > div > a > img:nth-child(1)';
+
+                const $selectedToon = $(selectorString);
+                const $ImageselectedToon = $(imgSelectorString);
+
+                //ToonList JSON Parsing
+                ToonList.toonList.push({
+                    idx : index,
+                    title : $($selectedToon).attr('title'),
+                    finished : '',
+                    thumbnail : $($ImageselectedToon).attr('src'),
+                    URL : baseURL+$($selectedToon).attr('href'),
+                });
+                // console.log(ToonList);
+            }
+            allToonList.toonList.push(ToonList);
+        }
+
+        //JSON Writing
+        const JSON_weekToonList = JSON.stringify(allToonList);
+        fs.writeFileSync('AllToonList.json',JSON_weekToonList);
+        
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    .then(function() {
+        
+    });
+
+    
